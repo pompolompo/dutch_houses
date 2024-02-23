3491399feb135835fab36847914264cd9b87152b
pompolompo <enferrofa@gmail.com>
2024-02-23 12:22:47 +0100

    Afegeixo les dades i la codificació de cadascuna

---
+++ SCRIPTS/descriptiva0.R
@@ -0,0 +1,9 @@
+# Metadata ----------------------------------------------------------------
+# written by: Ferran Garcia, @pompolompo
+# written on: 23-02-2024
+# purpose: decribe dataset
+# description: exploratory decription before imputation of NA's
+
+
+
+
---
+++ SCRIPTS/import.R
@@ -0,0 +1,31 @@
+# Metadata ----------------------------------------------------------------
+# written by: Ferran Garcia, @pompolompo
+# written on: 23-02-2024
+# purpose: import dataset
+# description: reads data of dutch housing and selects random subset of observation
+
+
+# Libraries ---------------------------------------------------------------
+library(readxl)
+
+
+# Global Options --------------------------------------------------------------
+wd <- "/home/ferran/Documents/Universitat/MULTI/dutch_houses"
+file <- "/dutch_houses.xlsx"
+
+arrel <- 12345
+subset_obs <- 5000
+
+# Import ------------------------------------------------------------------
+tbl_houses <- readxl::read_excel(
+  paste0(wd, "/DATA", file)
+  )
+
+
+# Subset ------------------------------------------------------------------
+set.seed(seed = arrel)
+selected_obs <- sample(x = 1:nrow(tbl_houses), 
+                       size = subset_obs,
+                       replace = FALSE)
+tbl_houses_subset <- tbl_houses[selected_obs, ]
+
