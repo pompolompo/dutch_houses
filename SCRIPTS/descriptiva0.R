ec5f8d9363a74810717ef506c2659d6da2d45fd2
pompolompo <enferrofa@gmail.com>
2024-02-24 14:15:01 +0100

    Update descriptiva0.R
    Afegeixo les seccions de l'scrip, identifico la tipologia de variables.
    
    Cal modificar la tipologia de les variables

--- SCRIPTS/descriptiva0.R
+++ SCRIPTS/descriptiva0.R
@@ -1,8 +1,58 @@
 # Metadata ----------------------------------------------------------------
 # written by: Ferran Garcia, @pompolompo
-# written on: 23-02-2024
+# written on: 24-02-2024
 # purpose: decribe dataset
 # description: exploratory decription before imputation of NA's
+
+
+# Libraries ---------------------------------------------------------------
+
+# Global Options ----------------------------------------------------------
+wd <- "/home/ferran/Documents/Universitat/MULTI/dutch_houses"
+file <- "/subset_houses.RData"
+  
+# Import ------------------------------------------------------------------
+load(
+  paste0(wd, "/DATA", file)
+)
+
+# Tipologia Variables -----------------------------------------------------
+
+str(tbl_houses_subset)
+
+num_var_ind <- sapply(
+  X = tbl_houses_subset,
+  FUN = is.numeric
+  ) |>
+  which()
+
+char_var_ind <- sapply(
+  X = tbl_houses_subset,
+  FUN = is.character
+) |>
+  which()
+
+fact_var_ind <- sapply(
+  X = tbl_houses_subset,
+  FUN = is.factor
+) |>
+  which()
+
+# @pompolompo, 24-02-2024
+# cal canviar la tipologia d'algunes variables
+# en general la majoria de strings han de ser factors
+
+# Descriptiva Univariant --------------------------------------------------
+
+## Variables numèriques ----------------------------------------------------
+
+## Variables categòriques --------------------------------------------------
+
+# Descriptiva bivariant ---------------------------------------------------
+
+## Variables numèriques ----------------------------------------------------
+
+## Variables categòriques --------------------------------------------------
 
 
 
