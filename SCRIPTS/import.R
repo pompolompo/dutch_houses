335a255759ff23158219e2b6c8456008e50405ef
eliesroman <161130884+eliesroman@users.noreply.github.com>
2024-02-25 18:31:31 +0100

    s'ha eliminat els valors on area floor=0
    S'ha fet un últim retoc a la importació de les dades ja que hi havia valors on
     floor_area era 0 (que no té sentit ja que no hi ha casa bàsicament) i per tant la prize per metre quadrat era infinit. S'ha filtrat aquesta variable per valors diferents de 0.
    
    Update import.R

--- SCRIPTS/import.R
+++ SCRIPTS/import.R
@@ -11,9 +11,12 @@
 # Libraries ---------------------------------------------------------------
 library(readxl)
 
+
 # Global Options --------------------------------------------------------------
 wd <- "/home/ferran/Documents/Universitat/MULTI/dutch_houses"
 wd<-"C:/Users/hp/Documents/dutch_houses"
+wd<-"C:/Users/maria/OneDrive/Desktop/multi"
+
 file <- "/dutch_houses.xlsx"
 outputname <- "subset_houses"
 
@@ -25,11 +28,21 @@ tbl_houses <- readxl::read_excel(
   paste0(wd, "/DATA", file)
   )
 
+
+
+# Noves variables ---------------------------------------------------------
+
+tbl_houses[,"price_metre"]<-tbl_houses[,"sale_price"]/tbl_houses[,"floor_area"]
+
+
+
 # Subset ------------------------------------------------------------------
 tbl_houses[["busy_street"]] <- gsub(
   x = tbl_houses[["busy_street"]],
   pattern = "2",
   replacement = "1")
+
+tbl_houses<-subset(tbl_houses, floor_area!=0)
 
 set.seed(seed = arrel)
 selected_obs <- sample(x = 1:nrow(tbl_houses), 
@@ -37,9 +50,6 @@ selected_obs <- sample(x = 1:nrow(tbl_houses),
                        replace = FALSE)
 tbl_houses_subset <- tbl_houses[selected_obs, ]
 
-# Noves variables ---------------------------------------------------------
-
-tbl_houses_subset[,"price_metre"]<-tbl_houses_subset[,"sale_price"]/tbl_houses_subset[,"floor_area"]
 
 # Save .Rdata -------------------------------------------------------------
 
@@ -47,3 +57,4 @@ save(
   tbl_houses_subset,
   file = paste0(wd, "/DATA/", outputname, ".RData")
     )
+
