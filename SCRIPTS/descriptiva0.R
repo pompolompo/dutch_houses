84959a987fc54d3e1b1965d34ec2e76d510aa241
claratg15 <161029466+claratg15@users.noreply.github.com>
2024-02-26 16:21:10 +0100

    Update descriptiva0.R
    Afegeixo l'anàlisi descriptiva (numèrica i gràfica) de les variables numèriques contínues

--- SCRIPTS/descriptiva0.R
+++ SCRIPTS/descriptiva0.R
@@ -47,6 +47,35 @@ fact_var_ind <- sapply(
 
 ## Variables numèriques ----------------------------------------------------
 
+### Numèriques contínues
+
+library(psych)
+library(kableExtra)
+
+desc_parcel_size <- as.data.frame(psych::describe(tbl_houses_subset[, "parcel_size"]))
+kable(desc_parcel_size, caption = "Anàlisi descriptiu: variable parcel_size") %>% kable_styling(full_width = FALSE)
+
+desc_floor_area <- as.data.frame(psych::describe(tbl_houses_subset[, "floor_area"]))
+kable(desc_floor_area, caption = "Anàlisi descriptiu: variable floor_area") %>% kable_styling(full_width = FALSE)
+
+desc_sale_price <- as.data.frame(psych::describe(tbl_houses_subset[, "sale_price"]))
+kable(desc_sale_price, caption = "Anàlisi descriptiu: variable sale_price") %>% kable_styling(full_width = FALSE)
+
+desc_price_metre <- as.data.frame(psych::describe(tbl_houses_subset[, "price_metre"]))
+kable(desc_price_metre, caption = "Anàlisi descriptiu: variable price_metre") %>% kable_styling(full_width = FALSE)
+
+
+library(ggplot2)
+num_var_ind_cont <- c(5,6,12,18)
+
+for(k in num_var_ind_cont){
+  print(ggplot(tbl_houses_subset, aes(x=!!sym(names(tbl_houses_subset)[k]))) + geom_histogram())
+}
+
+for(k in num_var_ind_cont){
+  print(ggplot(tbl_houses_subset, aes(x="", y=!!sym(names(tbl_houses_subset)[k]))) + geom_boxplot())
+}
+
 ## Variables categòriques --------------------------------------------------
 
 # Descriptiva bivariant ---------------------------------------------------
