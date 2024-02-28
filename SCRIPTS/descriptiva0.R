3e297bb7b2db87ea5480475ae7ad391150644b92
eliesroman <161130884+eliesroman@users.noreply.github.com>
2024-02-28 23:27:32 +0100

    Anàlisi bivariant
    S'ha fet una taula de contingencia, un mosaicplot i un barplot bivariant per totes les combinacions de variables catagoriques. S'han exclos les variables categoriques identificadores com IDHOUSE i Codi_Postal ja que careixen de sentit i no tenen cap valor informatiu.

--- SCRIPTS/descriptiva0.R
+++ SCRIPTS/descriptiva0.R
@@ -123,6 +123,357 @@ table(tbl_houses_subset[,"busy_street"])
 
 ## Variables categòriques --------------------------------------------------
 
+colors <- c("lightblue", "lightgreen", "lightpink", "lightyellow", "lightcyan", 
+                     "lightgray", "lightcoral", "lightseagreen", "lightsalmon")
+
+
+library(kableExtra)
+
+
+### Any i condició interior
+
+a<-table(tbl_houses_subset$year,tbl_houses_subset$interior_condition)
+
+kable(a, caption = "Taula de Contingencia: Year vs. Interior Condition")
 
 
 
+ggplot(tbl_houses_subset, aes(x = year, fill = interior_condition)) +
+         geom_bar(position = "dodge", width = 0.7) +
+         labs(x = "year", y = "Conteig", fill = "interior condition") +
+         ggtitle("Gràfic de barres bivariant entre year i interior condition") +
+         theme_minimal()
+
+
+mosaicplot(table(tbl_houses_subset$year,tbl_houses_subset$interior_condition), main = "Mosaic Plot  entre year i interior condition",sub="Year", col=colors, ylab="Interior Condition")
+
+
+
+### Energy label i Condició interior
+
+a<-table(tbl_houses_subset$energy_label,tbl_houses_subset$interior_condition)
+
+kable(a, caption = "Taula de Contingencia: Energy Label vs. Interior Condition")
+
+ggplot(tbl_houses_subset, aes(x = energy_label, fill = interior_condition)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "Energy_label", y = "Conteig", fill = "interior_condition") +
+  ggtitle("Gràfic de barres bivariant entre Energy label i interior_condition") +
+  theme_minimal()
+
+
+
+mosaicplot(table(tbl_houses_subset$energy_label,tbl_houses_subset$interior_condition), main = "Mosaic Plot entre energy label i interior condition", sub="Energy_Label", col=colors, ylab="Interior condition")
+
+
+### Condició exteriors i Energy label
+
+a<-table(tbl_houses_subset$energy_label,tbl_houses_subset$exterior_condition)
+
+
+kable(a, caption = "Taula de Contingencia: Energy Label vs. Exterior Condition")
+
+
+ggplot(tbl_houses_subset, aes(x = energy_label, fill = exterior_condition)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "Energy_label", y = "Conteig", fill = "Exterior condition") +
+  ggtitle("Gràfic de barras bivariant entre  Energy label i Exterior condition") +
+  theme_minimal()
+
+
+
+mosaicplot(table(tbl_houses_subset$energy_label,tbl_houses_subset$exterior_condition), main = "Mosaic Plot entre Energy label i Exterior condition", col=colors, ylab="Exterior condition")
+
+### Any i Energy label
+
+
+
+a<- table(tbl_houses_subset$year,tbl_houses_subset$energy_label)
+
+kable(a, caption = "Taula de Contingencia: Year vs. Energy Label  ")
+
+
+ggplot(tbl_houses_subset, aes(x = year, fill = energy_label)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "Year", y = "Conteig", fill = "Energy Label ") +
+  ggtitle("Gràfic de barras bivariant entre  year i energy label") +
+  theme_minimal()
+
+
+
+mosaicplot(table(tbl_houses_subset$year,tbl_houses_subset$energy_label), main = "Mosaic Plot year i Energy label", col=colors)
+
+###City i year
+taula<-table(tbl_houses_subset$city,tbl_houses_subset$year)
+kable(taula, caption = "Tabla de Contingencia: City vs. Year")
+
+ggplot(tbl_houses_subset, aes(x = city, fill = year)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "city",fill = "year") +
+  ggtitle("Gráfic de barres bivariant entre city i year") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$city,
+                 tbl_houses_subset$year), 
+           main = "Mosaic Plot", sub="city", col=colors,ylab = "year")
+
+
+###City i energy_label
+taula<-table(tbl_houses_subset$city,tbl_houses_subset$energy_label)
+kable(taula, caption = "Tabla de Contingencia: City vs. Energy Label")
+
+ggplot(tbl_houses_subset, aes(x = city, fill = energy_label)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "city",fill = "energy_label") +
+  ggtitle("Gráfic de barres bivariant entre city i energy_label") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$city,
+                 tbl_houses_subset$energy_label), 
+           main = "Mosaic Plot", sub="city", col=colors,ylab = "energy_label")
+
+###Type i year
+taula<-table(tbl_houses_subset$type,tbl_houses_subset$year)
+kable(taula, caption = "Tabla de Contingencia: Type vs. Year")
+
+ggplot(tbl_houses_subset, aes(x = type, fill = year)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "type",fill = "year") +
+  ggtitle("Gráfic de barres bivariant entre type i year") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$type,
+                 tbl_houses_subset$year), 
+           main = "Mosaic Plot", sub="type", col=colors,ylab = "year")
+
+###Type i energy_label
+taula<-table(tbl_houses_subset$type,tbl_houses_subset$energy_label)
+kable(taula, caption = "Tabla de Contingencia: Type vs. Energy Label")
+
+ggplot(tbl_houses_subset, aes(x = type, fill = energy_label)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "type", fill = "energy_label") +
+  ggtitle("Gráfic de barres bivariant entre type i energy_label") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$type,
+                 tbl_houses_subset$energy_label), 
+           main = "Mosaic Plot", sub="type", col=colors,ylab = "energy_label")
+
+###Construction_period i year
+taula<-table(tbl_houses_subset$construction_period,tbl_houses_subset$year)
+kable(taula, caption = "Tabla de Contingencia: Construction_period vs. Year")
+
+ggplot(tbl_houses_subset, aes(x = construction_period, fill = year)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "construction_period", fill = "year") +
+  ggtitle("Gráfic de barres bivariant entre construction Period i year") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$construction_period,
+                 tbl_houses_subset$year), 
+           main = "Mosaic Plot", sub="construction_period", col=colors,ylab = "year")
+
+###Construction_period i energy_label
+taula<-table(tbl_houses_subset$construction_period,tbl_houses_subset$energy_label)
+kable(taula, caption = "Tabla de Contingencia: Construction Period vs. Energy Label")
+
+ggplot(tbl_houses_subset, aes(x = construction_period, fill = energy_label)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "construction_period", fill = "energy_label") +
+  ggtitle("Gráfic de barres bivariant entre construction_period i energy_label") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$construction_period,
+                 tbl_houses_subset$energy_label), 
+           main = "Mosaic Plot", sub="construction_period", col=colors,ylab = "energy_label")
+
+### Year i exterior condition
+taula<-table(tbl_houses_subset$year,tbl_houses_subset$exterior_condition)
+kable(taula, caption = "Tabla de Contingencia: Construction Period vs. Energy Label")
+
+ggplot(tbl_houses_subset, aes(x = year, fill = exterior_condition)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "year", fill = "exterior_condition") +
+  ggtitle("Gráfic de barres bivariant entre year i exterior_condition") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$year,
+                 tbl_houses_subset$exterior_condition), 
+           main = "Mosaic Plot", sub="year", col=colors,ylab = "exterior_condition")
+
+###Construction_period i interior_condition
+taula<-table(tbl_houses_subset$construction_period,tbl_houses_subset$interior_condition)
+kable(taula, caption = "Tabla de Contingencia: Construction Period vs. Interior Condition")
+
+ggplot(tbl_houses_subset, aes(x = construction_period, fill = interior_condition)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "construction_period", fill = "interior_condition") +
+  ggtitle("Gráfic de barres bivariant entre construction_period i interior_condition") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$construction_period,
+                 tbl_houses_subset$interior_condition), 
+           main = "Mosaic Plot", sub="construction_period", col=colors,ylab = "interior_condition")
+
+###Construction_period i exterior_condition
+taula<-table(tbl_houses_subset$construction_period,tbl_houses_subset$exterior_condition)
+kable(taula, caption = "Tabla de Contingencia: Construction Period vs. Exterior Condition")
+
+ggplot(tbl_houses_subset, aes(x = construction_period, fill = exterior_condition)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "construction_period", fill = "exterior_condition") +
+  ggtitle("Gráfic de barres bivariant entre construction_period i exterior_condition") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$construction_period,
+                 tbl_houses_subset$exterior_condition), 
+           main = "Mosaic Plot", sub="construction_period", col=colors,ylab = "exterior_condition")
+
+###Interior_condition i exterior_condition
+taula<-table(tbl_houses_subset$interior_condition,tbl_houses_subset$exterior_condition)
+kable(taula, caption = "Tabla de Contingencia: Interior Condition vs. Exterior Condition")
+
+ggplot(tbl_houses_subset, aes(x = interior_condition, fill = exterior_condition)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "interior_condition", fill = "exterior_condition") +
+  ggtitle("Gráfic de barres bivariant entre interior_condition i exterior_condition") +
+  theme_minimal()
+
+mosaicplot(table(tbl_houses_subset$interior_condition,
+                 tbl_houses_subset$exterior_condition), 
+           main = "Mosaic Plot", sub="interior_condition", col=colors,ylab = "exterior_condition")
+
+
+###city - type
+
+a<-table(tbl_houses_subset$city,tbl_houses_subset$type)
+kable(a, caption = "Tabla de Contingencia: city vs. type")
+
+
+ggplot(tbl_houses_subset, aes(x = city, fill = type)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "city", y = "Freqüència", fill = "type") +
+  ggtitle("Gràfica de Barres Bivariant") +
+  theme_minimal()
+
+
+
+mosaicplot(table(tbl_houses_subset$city,
+                 tbl_houses_subset$type), 
+           main = "Mosaic Plot", sub="city", col=colors,ylab = "type")
+
+
+
+###city-construction_period
+
+a<-table(tbl_houses_subset$city,tbl_houses_subset$construction_period)
+kable(a, caption = "Tabla de Contingencia: city vs. construction_period")
+
+ggplot(tbl_houses_subset, aes(x = city, fill = construction_period)) +
+  geom_bar(position = "dodge", width = 0.7) +
+  labs(x = "city", y = "Freqüència", fill = "construction_period") +
+  ggtitle("Gràfica de Barres Bivariant") +
+  theme_minimal()
+
+
+mosaicplot(table(tbl_houses_subset$city,
+                 tbl_houses_subset$construction_period), 
+           main = "Mosaic Plot", sub="city", col=colors,
+           ylab = "construction_period")
+
+### city - condicions habitatge
+
+a<-table(tbl_houses_subset$city,tbl_houses_subset$interior_condition)
+kable(a, caption = "Tabla de Contingencia: city vs. interior_condition")
+
+ggplot(tbl_houses_subset, aes(x = city, fill = interior_condition)) +
+  geom_bar(position = "dodge",, width = 0.7) +
+  labs(x = "city", y = "Freqüència", fill = "interior_condition") +
+  ggtitle("Gràfica de Barres Bivariant") +
+  theme_minimal()
+
+
+
+mosaicplot(table(tbl_houses_subset$city,
+                 tbl_houses_subset$interior_condition), 
+           main = "Mosaic Plot", sub="city", col=colors,
+           ylab = "interior_condition")
+
+
+###city - condicions exterior
+
+
+a<-table(tbl_houses_subset$city,tbl_houses_subset$exterior_condition)
+kable(a, caption = "Tabla de Contingencia: city vs. exterior_condition")
+
+ggplot(tbl_houses_subset, aes(x = city, fill = exterior_condition)) +
+  geom_bar(position = "dodge",, width = 0.7) +
+  labs(x = "city", y = "Freqüència", fill = "exterior_condition") +
+  ggtitle("Gràfica de Barres Bivariant") +
+  theme_minimal()
+
+
+
+mosaicplot(table(tbl_houses_subset$city,
+                 tbl_houses_subset$exterior_condition), 
+           main = "Mosaic Plot", sub="city", col=colors,
+           ylab = "exterior_condition")
+
+
+###type - construction period
+
+
+a<-table(tbl_houses_subset$type,tbl_houses_subset$construction_period)
+kable(a, caption = "Tabla de Contingencia: type vs. construction_period")
+
+
+
+ggplot(tbl_houses_subset, aes(x = type, fill = construction_period)) +
+  geom_bar(position = "dodge",, width = 0.7) +
+  labs(x = "type", y = "Freqüència", fill = "construction_period") +
+  ggtitle("Gràfica de Barres Bivariant") +
+  theme_minimal()
+
+
+mosaicplot(table(tbl_houses_subset$type,
+                 tbl_houses_subset$construction_period), 
+           main = "Mosaic Plot", sub="type", col=colors,
+           ylab = "construction_period")
+
+
+###type - interior_conditon
+
+a<-table(tbl_houses_subset$type,tbl_houses_subset$interior_condition)
+kable(a, caption = "Tabla de Contingencia: type vs. interior_condition")
+
+ggplot(tbl_houses_subset, aes(x = type, fill = interior_condition)) +
+  geom_bar(position = "dodge",, width = 0.7) +
+  labs(x = "type", y = "Freqüència", fill = "interior_condition") +
+  ggtitle("Gràfica de Barres Bivariant") +
+  theme_minimal()
+
+
+
+mosaicplot(table(tbl_houses_subset$type,
+                 tbl_houses_subset$interior_condition), 
+           main = "Mosaic Plot", sub="type", col=colors,
+           ylab = "interior_condition")
+
+###type - exterior condition
+
+
+a<-table(tbl_houses_subset$type,tbl_houses_subset$exterior_condition)
+kable(a, caption = "Tabla de Contingencia: type vs. exterior_condition")
+
+ggplot(tbl_houses_subset, aes(x = type, fill = exterior_condition)) +
+  geom_bar(position = "dodge",, width = 0.7) +
+  labs(x = "type", y = "Freqüència", fill = "exterior_condition") +
+  ggtitle("Gràfica de Barres Bivariant") +
+  theme_minimal()
+
+
+mosaicplot(table(tbl_houses_subset$type,
+                 tbl_houses_subset$exterior_condition), 
+           main = "Mosaic Plot", sub="type", col=colors,
+           ylab = "exterior_condition")
