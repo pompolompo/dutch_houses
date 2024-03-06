831ef2694678e13c7b66e9a34fb88cadd72e9154
@silrovira <silviarovira2003mail.com>
2024-03-06 16:44:20 +0100

    hem canviat els 0 de floor_area i els hem posat com a NA

--- SCRIPTS/preprocessing.R
+++ SCRIPTS/preprocessing.R
@@ -1,6 +1,8 @@
 #PREPROCESSING:
 # modified by: Sílvia Rovira, @silrovira
 # modified on: 06-03-2024
+# modified by: Clara Tarragó, @claratg15
+# modified on: 06-03-2024
 
 #### Afegir NA-----------------------------------------------------------
 
@@ -14,9 +16,16 @@ for(i in 1:length(num)){
   tbl_houses_subset[cat,"interior_condition"]<- NA
 }
 
-wd<-"C:/Users/hp/Documents/dutch_houses"
+###Agafar els 0 de la variable floor_area i passar-los a NA
+
+for (i in 1:dim(tbl_houses_subset)[1]){
+  if(tbl_houses_subset[i,"floor_area"]==0){
+    tbl_houses_subset[i,"floor_area"]<-NA
+  }
+}
 
 # Save .Rdata -------------------------------------------------------------
+wd<-"C:/Users/hp/Documents/dutch_houses"
 save(
   tbl_houses_subset,
   file = paste0(wd, "/DATA/", outputname, ".RData")
