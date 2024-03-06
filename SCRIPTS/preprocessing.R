4c83872b29a5cc524a7e74bcb69f48371e0e8262
claratg15 <161029466+claratg15@users.noreply.github.com>
2024-03-06 17:10:24 +0100

    Update preprocessing.R
    He afegit el MICE i el Test de Little. Little no em surt del tot bé, per si algu s'ho pot mirar.

--- SCRIPTS/preprocessing.R
+++ SCRIPTS/preprocessing.R
@@ -4,19 +4,24 @@
 # modified by: Clara Tarragó, @claratg15
 # modified on: 06-03-2024
 
+#### Carregar les dades -------------------------------------------------
+
+load("D:/3rCURS/2. anàlisi multivariant/treball/subset_houses.RData")
+
 #### Afegir NA-----------------------------------------------------------
 
-set.seed(arrel)
+set.seed(12345)
 num<- sample(5000,20)
-set.seed(arrel)
+set.seed(12345)
 cat<- sample(5000,20)
 
+### Afegim NAs a les variables time_on_market i interior_condition
 for(i in 1:length(num)){
   tbl_houses_subset[num[i],"time_on_market"]<- NA
-  tbl_houses_subset[cat,"interior_condition"]<- NA
+  tbl_houses_subset[cat[i],"interior_condition"]<- NA
 }
 
-###Agafar els 0 de la variable floor_area i passar-los a NA
+### Agafem els 0 de la variable floor_area, que no tenen sentit, i els passem a NA
 
 for (i in 1:dim(tbl_houses_subset)[1]){
   if(tbl_houses_subset[i,"floor_area"]==0){
@@ -24,9 +29,40 @@ for (i in 1:dim(tbl_houses_subset)[1]){
   }
 }
 
-# Save .Rdata -------------------------------------------------------------
-wd<-"C:/Users/hp/Documents/dutch_houses"
+### Eliminem la variable price_metre. Una vegada imputem els valors NA de floor_area, la tornarem a crear
+tbl_houses_subset <- tbl_houses_subset[,1:17]
+
+### Save .Rdata -------------------------------------------------------------
+wd <- "D:/3rCURS/2. anàlisi multivariant/treball/"
+outputname <- "subset_houses_NA"
+
 save(
   tbl_houses_subset,
-  file = paste0(wd, "/DATA/", outputname, ".RData")
+  file = paste0(wd, outputname, ".RData")
 )
+
+### Test de Little -------------------------------------------------------------
+### Per verificar que els Nas esiguin distribuits aleatòriament.
+
+# install.packages("naniar")
+library(naniar)
+mcar_test(tbl_houses_subset)
+# no sé perquè em surt p-valor de 0. Si algú s'ho pot mirar porfi
+
+### MICE ----------------------------------------------------------------------
+
+# install.packages("mice")
+library(mice)
+
+### Variables numèriques: time_on_market i floor_area
+imp <- mice(tbl_houses_subset, m = 5, maxit = 50, meth = 'pmm', seed= 12345)
+tbl_houses_subset <- complete(imp,1)
+
+### Variable categòrica: interior_condition
+tbl_houses_subset$interior_condition <- as.numeric(tbl_houses_subset$interior_condition)
+
+imp <- mice(tbl_houses_subset, m = 5, maxit = 50, meth = 'pmm', seed= 12345)
+tbl_houses_subset <- complete(imp,1)
+
+
+
