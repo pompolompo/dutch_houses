311fcfbeeac05fee178deeb9b1a3c900cf09799f
pompolompo <enferrofa@gmail.com>
2024-03-28 10:39:40 +0100

    Create profiling.R
    Copio el codi de profiling que hi havia a l'script de clustering

---
+++ SCRIPTS/profiling.R
@@ -0,0 +1,20 @@
+#PREPROFILING
+#tbl_houses_subset$grups <- as.character(cutree(
+#  tree = h,
+#  k = 4
+#))
+
+#library(FactoMineR)
+#plot(FactoMineR::catdes(tbl_houses_subset, num.var = 19))
+
+# PREPROFILING
+
+#tbl_houses_subset$grups <- as.character(cutree(
+#  tree = h2,
+#  k = 6
+#))
+
+# library(FactoMineR)
+# sense_id <- tbl_houses_subset[,c(1:14,17:19)]
+# num <- tbl_houses_subset[,c(numeriques,19)]
+# plot(FactoMineR::catdes(num, num.var = 7))
